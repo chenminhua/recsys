5056478412310a1e4af63816c302deea2becedc1
chenminhua <chenmhgo@gmail.com>
2019-12-24 13:46:55 +0800

    dataset site

--- getdata.sh
+++ getdata.sh
@@ -1,3 +1,4 @@
+# https://grouplens.org/datasets/movielens/
 wget http://files.grouplens.org/datasets/movielens/ml-1m.zip
 wget http://files.grouplens.org/datasets/movielens/ml-100k.zip
 wget http://files.grouplens.org/datasets/movielens/ml-25m.zip
