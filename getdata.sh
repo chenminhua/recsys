0bde0e2bd0629f2803d6751391e3b9843f1d55e6
chenminhua <chenmhgo@gmail.com>
2019-12-24 13:40:10 +0800

    UserCF

---
+++ getdata.sh
@@ -0,0 +1,3 @@
+wget http://files.grouplens.org/datasets/movielens/ml-1m.zip
+wget http://files.grouplens.org/datasets/movielens/ml-100k.zip
+wget http://files.grouplens.org/datasets/movielens/ml-25m.zip
